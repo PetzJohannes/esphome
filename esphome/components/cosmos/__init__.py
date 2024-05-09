472c27824db16860cc941b26bae96c80a79a53a3
Johannes Petz <johannes.petz@redmedical.de>
2024-05-09 23:48:36 +0200

    wip

--- esphome/components/cosmos/__init__.py
+++ esphome/components/cosmos/__init__.py
@@ -2,8 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import automation
 from esphome.automation import maybe_simple_id
-from esphome.components import fan, output
-from esphome.components.fan import validate_preset_modes
+from esphome.components import empty_fan, output
+from esphome.components.empty_fan import validate_preset_modes
 from esphome.const import (
     CONF_ID,
     CONF_DECAY_MODE,
@@ -19,7 +19,7 @@ CODEOWNERS = ["@PetzJohannes"]
 
 
 cosmos_ns = cg.esphome_ns.namespace("cosmos")
-CosmosFan = cosmos_ns.class_("CosmosFan", cg.Component, fan.Fan)
+CosmosFan = cosmos_ns.class_("CosmosFan", cg.Component, empty_fan.Fan)
 
 DecayMode = cosmos_ns.enum("DecayMode")
 DECAY_MODE_OPTIONS = {
@@ -30,7 +30,7 @@ DECAY_MODE_OPTIONS = {
 # Actions
 BrakeAction = cosmos_ns.class_("BrakeAction", automation.Action)
 
-CONFIG_SCHEMA = fan.FAN_SCHEMA.extend(
+CONFIG_SCHEMA = empty_fan.FAN_SCHEMA.extend(
     {
         cv.GenerateID(CONF_ID): cv.declare_id(CosmosFan),
         cv.Required(CONF_PIN_A): cv.use_id(output.FloatOutput),
@@ -62,7 +62,7 @@ async def to_code(config):
         config[CONF_DECAY_MODE],
     )
     await cg.register_component(var, config)
-    await fan.register_fan(var, config)
+    await empty_fan.register_fan(var, config)
     pin_a_ = await cg.get_variable(config[CONF_PIN_A])
     cg.add(var.set_pin_a(pin_a_))
     pin_b_ = await cg.get_variable(config[CONF_PIN_B])
