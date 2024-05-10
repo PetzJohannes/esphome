4ddba08c8e0f5d3fb7003f95c140ddeb11fabfd0
Johannes Petz <johannes.petz@redmedical.de>
2024-05-10 20:44:54 +0200

    wip

--- esphome/components/cosmos/fan/__init__.py
+++ esphome/components/cosmos/fan/__init__.py
@@ -19,7 +19,7 @@ print("TEST cosmos")
 
 CODEOWNERS = ["@PetzJohannes"]
 
-cosmos_ns = cg.esphome_ns.namespace("fan.cosmos")
+cosmos_ns = cg.esphome_ns.namespace("cosmos")
 CosmosFan = cosmos_ns.class_("CosmosFan", cg.Component, fan.Fan)
 
 DecayMode = cosmos_ns.enum("DecayMode")
